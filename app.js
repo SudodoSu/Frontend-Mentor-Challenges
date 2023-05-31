f35f0e263b5c96c17526558576b1447998915386
SudodoSu <sudodosu99@gmail.com>
2023-05-31 21:14:26 +0200

    Error message added to the sign up form

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-const form = document.getElementById("signUpForm")
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll('input');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
-    const firstNameInput = form.elements.name
+const errorMess = document.querySelectorAll(".error-mess")
 
-    if (!firstNameInput.checkValidity()) {
-        firstNameInput.classList.add('invalid')
-    }
-    else {
-        firstNameInput.classList.remove('invalid')
-    }
+const RegExps = [/^[0-9a-zA-Z]{3,15}$/, 
+                /^[0-9a-zA-Z]{3,15}$/,                                              
+                /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$/,           
+                /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.{8,})/,                              
+            ]
 
-    if (form.chackValidity()) {
-        form.submit()
-    }
-})
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    inputs.forEach((input, index) => {
+
+        const currentRegExp = RegExps[index]
+
+        if (!input.value) {
+            input.classList.add('invalid-input');
+            errorMess[index].style.display = 'block';
+        } else {
+            input.classList.remove('invalid-input');
+            errorMess[index].style.display = 'none';
+        }
+
+        input.addEventListener('keyup', (e)=> {
+            
+            const inputText = e.target.value;
+
+            if (currentRegExp.test(inputText)) {
+                e.target.classList.remove("invalid-input")
+                errorMess[index].style.display = 'none';
+            } else {
+                e.target.classList.add("invalid-input")
+            }
+        })
+    });
+});
